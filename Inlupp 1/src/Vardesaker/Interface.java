620a42ee8f01027f88df22fde050388fef657a77
Lowqe <Lowqe@users.noreply.github.com>
2018-03-29 14:32:44 +0200

    Fönster påbörjat lel

---
+++ Inlupp 1/src/Vardesaker/Interface.java
@@ -0,0 +1,29 @@
+package Vardesaker;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Interface extends JFrame implements ActionListener {
+	private JTextField fält;
+	
+	public void actionPerformed(ActionEvent ave) {
+		String str =fält.getText();
+	}
+	
+	public Interface () {
+		super ("Värdesaker");
+		setLayout(new FlowLayout());
+		
+		JLabel namnLabel = new JLabel("Ange Värdesak:");
+		add(namnLabel);
+		fält = new JTextField(8);
+		add(fält);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(200, 100);
+		setVisible(true);
+	}
+	
+	public static void main (String [] args) {
+		new Interface ();
+	}
+}
